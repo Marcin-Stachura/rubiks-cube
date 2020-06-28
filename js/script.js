8193b5ce5a8402eb7e846b7e5f7dabafa53dddf8
Marcin Stachura <marcin.stachura6@gmail.com>
2020-06-28 23:46:51 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("Siemanko!");
+
+let tableButton = document.querySelector(".tableButton");
+let tableBody = document.querySelector(".tableBody");
+
+function tableShown() {
+  tableButton.innerText = "Porównanie opisanych wyżej metod - ukryj"
+  tableBody.style.display = "block";
+}
+
+function tableHidden() {
+  tableButton.innerText = "Porównanie opisanych wyżej metod - pokaż"
+  tableBody.style.display = "none";
+}
+
+tableHidden();
+
+tableButton.addEventListener("click", () => {
+  if (tableBody.style.display === "block") {
+    tableHidden();
+  }
+  else {
+    tableShown();
+  }
+});
